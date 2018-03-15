2499b5ac0b893f55dd3a7604055863052c2acaf3
Renata <re_srs@yahoo.com.br>
2018-03-15 01:31:00 -0300

    primeira versao

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CadastroComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
